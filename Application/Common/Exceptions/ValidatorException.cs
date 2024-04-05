179357810f595c972bb5d66bbb4d14923b9bf434
Saidjon <saidjonnozimboyevv@gmail.com>
2024-04-05 17:49:43 +0500

    services added

---
+++ Application/Common/Exceptions/ValidatorException.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Common.Exceptions;
+
+public class ValidatorException : Exception
+{
+    public ValidatorException(string message)
+        : base(message)
+    {
+        
+    }
+}
---
+++ Application/Services/GenreService.cs
@@ -0,0 +1,40 @@
+﻿using Application.DTOs.GenreDTOs;
+using Application.Interfaces;
+using Data.Interfaces;
+using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Services;
+
+public class GenreService(IUnitOfWork unitOfWork,
+                          IValidator<Genre> validator) 
+    : IGenreService
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IValidator<Genre> _validator = validator;
+
+    public Task CreateAsync(AddGenreDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<GenreDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GenreDto?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(GenreDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
