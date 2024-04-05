74d79a8cf3ba08072ddd35d1a352a0d49f69b69d
Saidjon <saidjonnozimboyevv@gmail.com>
2024-04-05 15:37:52 +0500

    minor change

--- MoviesWebAPI/Program.cs
+++ MoviesWebAPI/Program.cs
@@ -49,7 +49,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.UseMiddleware<ExceptionsHandler>();
