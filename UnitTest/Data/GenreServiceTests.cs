79285f1ac989dda6665e3cc90bd908f9698b5ced
Saidjon <saidjonnozimboyevv@gmail.com>
2024-05-20 15:01:44 +0500

    Moq added

---
+++ UnitTest/Data/GenreServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Application.Common.Exceptions;
+using Application.DTOs.GenreDTOs;
+using Application.Interfaces;
+using Application.Services;
+using Data.Interfaces;
+using Domain.Entities;
+using FluentValidation;
+using Moq;
+using System.Net;
+
+namespace UnitTest.Data;
+
+public class GenreServiceTests
+{
+    private Mock<IUnitOfWork> mockUnitOfWork = new();
+    private Mock<IValidator<Genre>> mockValidator = new();
+    private IUnitOfWork unitOfWork;
+    private IValidator<Genre> validator;
+    private IGenreService service;
+
+    [SetUp]
+    public void Setup()
+    {
+        unitOfWork = mockUnitOfWork.Object;
+        validator = mockValidator.Object;
+
+        service = new GenreService(unitOfWork, validator);
+    }
+
+    [Test]
+    public async Task AddGenreAsync_WhenGenreAlreadyExists_ThrowsStatusCodeException()
+    {
+        var dto = new AddGenreDto() { Name = "Test1" };
+        mockUnitOfWork.Setup(x => x.Genre.GetByNameAsync(dto.Name)).ReturnsAsync(new Genre());
+
+        var exception = Assert.ThrowsAsync<StatusCodeException>(() => service.CreateAsync(dto));
+
+        Assert.That(exception.StatusCode, Is.EqualTo(HttpStatusCode.AlreadyReported));
+        Assert.That(exception.Message, Is.EqualTo($"{dto.Name}"));
+    }
+
+}
--- UnitTest/Data/UserServiceTest.cs
+++ UnitTest/Data/UserServiceTest.cs
@@ -7,7 +7,7 @@ using UnitTest.Helpers;
 
 namespace UnitTest.Data;
 
-public class UserRepositoryTest
+public class UserServiceTest
 {
     AppDbContext dbContext;
     IUnitOfWork unitOfWork;
@@ -28,7 +28,7 @@ public class UserRepositoryTest
     [TestCase("Test6")]
     [TestCase("Test7")]
 
-    public async Task AddAsync(string name)
+    public async Task AddUserAsync(string name)
     {
         var user = new User()
         {
@@ -47,6 +47,54 @@ public class UserRepositoryTest
         var result = await dbContext.Users.FirstOrDefaultAsync(x => x.FirstName == name);
 
         Assert.That(user.FirstName, Is.EqualTo(result!.FirstName));
+    }
+
+    [Test]
+    [TestCase("Test1")]
+    [TestCase("Test2")]
+    [TestCase("Test3")]
+    [TestCase("Test4")]
+    [TestCase("Test5")]
+    [TestCase("Test6")]
+    [TestCase("Test7")]
+
+    public async Task AddGenreAsync(string name)
+    {
+        var genre = new Genre()
+        {
+            CreatedAt = DateTime.Now,
+            Description = "Description",
+            Name = name,
+        };
+
+        await unitOfWork.Genre.CreateAsync(genre);
+
+        var result = await dbContext.Genres.FirstOrDefaultAsync(x => x.Name == name);
+
+        Assert.That(genre.Name, Is.EqualTo(result!.Name));
+    }
+
+    [Test]
+    [TestCase("Test1")]
+    [TestCase("Test2")]
+    [TestCase("Test3")]
+    [TestCase("Test4")]
+    [TestCase("Test5")]
+    [TestCase("Test6")]
+    [TestCase("Test7")]
+
+    public async Task AddMovieAsync(string name)
+    {
+        var movie = new Movie()
+        {
+            Company = name,
+        };
+
+        await unitOfWork.Movie.CreateAsync(movie);
+
+        var result = await dbContext.Movies.FirstOrDefaultAsync(x => x.Company == name);
+
+        Assert.That(movie.Company, Is.EqualTo(result!.Company));
     }
 
     [TearDown]
